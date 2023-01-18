af9ce611421db2233f609e28aa35d4b3393de0fe
Muhammed-Rashin <ceorashin@gmail.com>
2023-01-18 14:33:25 +0530

    cookie testing

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'http://13.231.164.178:5000',
-  withCredentials: true,
+  // withCredentials: true,
+  credentials: 'include',
   headers: {
     token: 'myToken',
   },
