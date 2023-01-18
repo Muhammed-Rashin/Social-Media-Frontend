63d921f38175690d941689c2f5d099f774bbe7d7
Muhammed-Rashin <ceorashin@gmail.com>
2023-01-18 22:49:31 +0530

    api url changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
-  // baseURL: 'http://13.231.164.178:5000',
+  // baseURL: 'http://localhost:5000',
+  baseURL: 'http://13.231.164.178:5000',
   withCredentials: true,
   headers: {
     token: localStorage.getItem('Accesstoken'),
